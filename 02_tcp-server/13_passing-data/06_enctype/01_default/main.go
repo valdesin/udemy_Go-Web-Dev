b99b8a1c2e06ceb05072b53f2c3f6525286a70f5
Vlad <vlad19943103@gmail.com>
2018-08-05 12:25:17 +0200

    Done Enctype

---
+++ 02_tcp-server/13_passing-data/06_enctype/01_default/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+type person struct {
+	FirstName string
+	LastName  string
+	Subsribed bool
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":9090", nil)
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+	bs := make([]byte, req.ContentLength)
+	req.Body.Read(bs)
+	body := string(bs)
+
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := tpl.ExecuteTemplate(res, "index.gohtml", body)
+	if err != nil {
+		http.Error(res, err.Error(), 500)
+		log.Fatalln(err)
+	}
+}
